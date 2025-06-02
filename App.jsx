53a42f5b29a0bcb8f3f3a23f942aaf8b0bc3bdcf
inouye165 <inouye165@gmail.com>
2025-06-02 12:03:54 -0700

    feat: Finished "Travel Journal: key prop"

--- App.jsx
+++ App.jsx
@@ -1,16 +1,28 @@
-import Entry from "./components/Entry"
 import Header from "./components/Header"
+import Entry from "./components/Entry"
+import data from "./data"
 
 export default function App() {
+    
+    const entryElements = data.map((entry) => {
+        return (
+            <Entry
+                img={entry.img}
+                title={entry.title}
+                country={entry.country}
+                googleMapsLink={entry.googleMapsLink}
+                dates={entry.dates}
+                text={entry.text}
+            />
+        )
+    })
+    
     return (
         <>
-        <Header />
-        <main className="container">
-            <Entry />
-            <Entry />
-            <Entry />
-        </main>
+            <Header />
+            <main className="container">
+                {entryElements}
+            </main>
         </>
-        
     )
 }
